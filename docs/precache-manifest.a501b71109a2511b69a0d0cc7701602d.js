0edb2304c6c2793f5cc289dab0e49fd2ee5f7e46
Lorenzo Gabarda <lorenzo.gabarda@shoresuite.com>
2019-02-08 11:29:12 +0800

    Added docs

---
+++ docs/precache-manifest.a501b71109a2511b69a0d0cc7701602d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d526b05e5814dbf96203",
+    "url": "/static/css/main.19901cd3.chunk.css"
+  },
+  {
+    "revision": "d526b05e5814dbf96203",
+    "url": "/static/js/main.d526b05e.chunk.js"
+  },
+  {
+    "revision": "7970b2822f599debf759",
+    "url": "/static/js/1.7970b282.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc853a72ec938f0595c48bb38e9c4b6c",
+    "url": "/index.html"
+  }
+];
